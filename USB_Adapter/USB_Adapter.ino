34b9349136199d4c73c52730495cb1975b40cdd3
Frank Gorham-Engard <frankgorhamengard@gmail.com>
2019-02-04 17:29:39 -0500

    Add TWI monitor.
    Receive test  data.

--- USB_Adapter/USB_Adapter.ino
+++ USB_Adapter/USB_Adapter.ino
@@ -15,6 +15,8 @@ AltSoftSerial altser;
 // D8 - AltSoftSerial RX
 // D9 - AltSoftSerial TX
 
+#include <Wire.h>
+
 #define BLINKLED_13        13 
 #define VINPIN_A0 A0
 unsigned long triggerTime;
@@ -22,6 +24,8 @@ const int panelLedArr[5] = {5,6,9,10,11}; //map of wired pins to LEDs
 int incomingChar;
 int uutChar;
 int charCount;
+
+void receiveEvent(int);
 
 /////////////////////////////////////////////////////////////////////////////////
 // called once at start
@@ -32,6 +36,9 @@ void setup(){
   while (!Serial) delay(50);  // wait for Arduino Serial Monitor
   Serial.println("Sparky USB Test Monitor");
   Serial.println(__DATE__);
+
+  Wire.begin(0x71);                // join i2c bus with address #9
+  Wire.onReceive(receiveEvent); // register event
 
   pinMode(13   , OUTPUT);
   digitalWrite(13   , HIGH);
@@ -45,6 +52,7 @@ void printHelp (void) {
   Serial.println("Select mode: ");
   Serial.println(" 1  600 baud serial");
   Serial.println(" 2  servo detect");
+  Serial.println(" R  show TWI data");
   Serial.print("?:");
 // Serial.println("  ");
 }
@@ -53,6 +61,7 @@ void printHelp (void) {
 void runSerialMonitor(void);
 void runServoDetect(void);
 void localCircuitTest(void);
+void runTWIrcv(void);
 
 /////////////////////  MAIN LOOP  /////////////////////////////
 void loop() {
@@ -75,8 +84,9 @@ void loop() {
           Serial.println(charCount);
           printHelp();
           break;
-         case '9': 
-          localCircuitTest();
+         case 'R': 
+         case 'r': 
+          runTWIrcv();
           printHelp();
           break;
          case 'H': 
@@ -150,4 +160,35 @@ void localCircuitTest(void) {
       Serial.print( value1 );
    }
   }
+}
+
+boolean showTWIdata;
+void runTWIrcv(void) {
+  Serial.println("---- show TWI data, q to quit------");
+  while ( Serial.read() != 'q' ) { // until quit is hit
+//    testnow = millis();
+//    // once per period 
+//    if ( testnow >= triggerTime ) {
+//      triggerTime = testnow + 100; //period
+//      int value1 = (analogRead(A1)+3)>>2;   
+//    Serial.print( value1 );
+      showTWIdata = true;
+//    }
+  }
+  showTWIdata = false;
+  
+}
+
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+void receiveEvent(int howMany)
+{
+  //digitalWrite(led, HIGH);       // briefly flash the LED
+  while(Wire.available() > 1) {  // loop through all but the last
+    char c = Wire.read();        // receive byte as a character
+    if ( showTWIdata ) Serial.print(c);             // print the character
+  }
+  int x = Wire.read();           // receive byte as an integer
+  if ( showTWIdata ) Serial.println(x);             // print the integer
+  //digitalWrite(led, LOW);
 }
